fc5c14e1843c087532f9bc39fb40f4875322e432
TommyApolinario <101909069+TommyApolinario@users.noreply.github.com>
2022-08-13 15:08:26 -0500

    Citas Mejora 1

--- src/app/citas/citas.module.ts
+++ src/app/citas/citas.module.ts
@@ -4,9 +4,11 @@ import { CitasComponent } from "./citas.component";
 import { SectionsComponent } from "../sections/sections.component";
 import { SharedModule } from "../shared/shared.module";
 import { SectionsModule } from "../sections/sections.module";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [CitasComponent],
-  imports: [CommonModule, SharedModule, SectionsModule],
+  imports: [CommonModule, SharedModule, SectionsModule, FormsModule, NgbModule],
 })
 export class CitasModule {}
